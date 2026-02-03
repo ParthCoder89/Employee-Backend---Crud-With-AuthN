72d8ad40de88a90a6f0a6d23e9831b68f0368920
ParthCoder89 <parthgupta5041@gmail.com>
2026-02-03 08:48:31 +0530

    Commit the Entity class with main class

---
+++ src/main/java/com/example/EmployeeManagement/with/AuthN/Crud/CrudEntity/EUser.java
@@ -0,0 +1,22 @@
+package com.example.EmployeeManagement.with.AuthN.Crud.CrudEntity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Employees")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String LastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
---
+++ src/test/java/com/example/EmployeeManagement/with/AuthN/EmployeeManagementWithAuthNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeManagement.with.AuthN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementWithAuthNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
