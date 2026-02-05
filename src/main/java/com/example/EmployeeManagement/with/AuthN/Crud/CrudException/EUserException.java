9df460480fafb2410daf00941ee01d043775ce0d
ParthCoder89 <parthgupta5041@gmail.com>
2026-02-05 08:50:36 +0530

    Commit the exception part of the Crud

---
+++ src/main/java/com/example/EmployeeManagement/with/AuthN/Crud/CrudException/EUserException.java
@@ -0,0 +1,7 @@
+package com.example.EmployeeManagement.with.AuthN.Crud.CrudException;
+
+public class EUserException extends RuntimeException {
+    public EUserException(String message) {
+        super(message);
+    }
+}
