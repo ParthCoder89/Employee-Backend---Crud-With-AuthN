149d4572b7a790bd63705ca8938439f65da12ccb
ParthCoder89 <parthgupta5041@gmail.com>
2026-02-09 20:49:07 +0530

    Commit the Auth User Exception Java Class of Project

---
+++ src/main/java/com/example/EmployeeManagement/with/AuthN/Auth/AuthException/AuserException.java
@@ -0,0 +1,7 @@
+package com.example.EmployeeManagement.with.AuthN.Auth.AuthException;
+
+public class AuserException extends RuntimeException {
+    public AuserException(String message) {
+        super(message);
+    }
+}
