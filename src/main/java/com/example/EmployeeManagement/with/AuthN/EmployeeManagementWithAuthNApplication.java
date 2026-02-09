808862884b406950a681f8ce1fa8bd0fdd14e153
ParthCoder89 <parthgupta5041@gmail.com>
2026-02-09 20:48:10 +0530

    Commit the Main Class in Project

---
+++ src/main/java/com/example/EmployeeManagement/with/AuthN/EmployeeManagementWithAuthNApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeManagement.with.AuthN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementWithAuthNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementWithAuthNApplication.class, args);
+	}
+
+}
