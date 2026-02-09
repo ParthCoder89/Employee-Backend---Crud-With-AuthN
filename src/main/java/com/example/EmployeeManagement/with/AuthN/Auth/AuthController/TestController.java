b4accc9e1d9aefe59e54c74975ec1d7a1fe89fb3
ParthCoder89 <parthgupta5041@gmail.com>
2026-02-09 22:13:16 +0530

    Some changes in Test Controller Class in the project

--- src/main/java/com/example/EmployeeManagement/with/AuthN/Auth/AuthController/TestController.java
+++ src/main/java/com/example/EmployeeManagement/with/AuthN/Auth/AuthController/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
     @GetMapping("/api/secure")
     public String secure(){
         return "Jwt Authenticated API";
